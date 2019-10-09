358027f0afadcf496d3ab7e75b200933f3fe72eb
Nicholas Moore <nicholas.s.moore@ttu.edu>
2019-10-09 13:39:09 -0500

    Add Jacobi function and test

--- src/MooreJuliaMultigrid.jl
+++ src/MooreJuliaMultigrid.jl
@@ -1,5 +1,22 @@
 module MooreJuliaMultigrid
 
-greet() = print("Hello World!")
+using LinearAlgebra
+
+"""
+Run the Jacobi method on matrix system Ax=b for a total of numiters iterations.
+
+    Start with an initial guess of x0
+"""
+function Jacobi(A,b,x0,numiters=1)
+    Dinv = inv(Diagonal(diag(A)))
+    R = Dinv*(A - Diagonal(diag(A)))
+    xnew = copy(x0)
+    for i = 1:numiters
+        xnew = Dinv*b - R*xnew
+    end
+    return xnew
+end
+
+export Jacobi
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using MooreJuliaMultigrid
+using LinearAlgebra
 using Test
 
 @testset "MooreJuliaMultigrid.jl" begin
     # Write your own tests here.
+    n = 100
+    h = 1/n
+    d = 2*ones(n)
+    dl = -1*ones(n-1)
+    A = Tridiagonal(dl,d,dl)/h^2
+    b = zeros(n)
+    x0 = rand(n)
+    @test norm(Jacobi(A,b,x0,100000)) < 0.001
 end
