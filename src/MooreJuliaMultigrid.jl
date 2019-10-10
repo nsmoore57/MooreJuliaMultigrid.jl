2036bf34c43f784b178166914c5d39a3ac8ad49b
nsmoore57 <14064733+nsmoore57@users.noreply.github.com>
2019-10-10 14:50:35 -0500

    Add TODOs for later

--- src/MooreJuliaMultigrid.jl
+++ src/MooreJuliaMultigrid.jl
@@ -6,6 +6,7 @@ using LinearAlgebra
 Run the Jacobi method on matrix system Ax=b for a total of numiters iterations.
 
     Start with an initial guess of x0
+TODO: Change this to a weighted Jacobi - can use UniformScaling Matrix to simplify the weighted part
 """
 function Jacobi(A,b,x0,numiters=1)
     Dinv = inv(Diagonal(diag(A)))
