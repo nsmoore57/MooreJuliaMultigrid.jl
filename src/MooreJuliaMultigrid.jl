0c493eb165f5d0c279446e82b2177203a160d150
Nicholas Moore <nicholas.s.moore@ttu.edu>
2019-10-10 20:34:21 -0500

    Make Jacobi a weighted Jacobi

--- src/MooreJuliaMultigrid.jl
+++ src/MooreJuliaMultigrid.jl
@@ -7,12 +7,12 @@ Run the Jacobi method on matrix system Ax=b for a total of numiters iterations.
 
     Start with an initial guess of x0
 """
-function Jacobi(A,b,x0,numiters=1)
-    Dinv = inv(Diagonal(diag(A)))
-    R = Dinv*(A - Diagonal(diag(A)))
+function Jacobi(A,b,x0,numiters=1,weight=1)
+    wDinv = weight*inv(Diagonal(diag(A)))
+    R = UniformScaling(1-weight) + wDinv*(A - Diagonal(diag(A)))
     xnew = copy(x0)
     for i = 1:numiters
-        xnew = Dinv*b - R*xnew
+        xnew = wDinv*b - R*xnew
     end
     return xnew
 end
