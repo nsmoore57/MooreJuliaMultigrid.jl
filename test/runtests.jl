e47e25f04ae9f8131c3696df23cbddc0cfc2669e
Nicholas Moore <nicholas.s.moore@ttu.edu>
2019-10-10 20:34:39 -0500

    Update test so that it's quicker and using weighted Jacobi

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,6 @@ using Test
     dl = -1*ones(n-1)
     A = Tridiagonal(dl,d,dl)/h^2
     b = zeros(n)
-    x0 = rand(n)
-    @test norm(Jacobi(A,b,x0,100000)) < 0.001
+    x0 = [.2*sin(3*x/n)+.7*sin(4*x/n)+.4*sin(10*x/n) for x = 1:n]
+    @test norm(Jacobi(A,b,x0,200,2/3)) < 0.001
 end
