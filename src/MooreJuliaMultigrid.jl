feefaf53a4c2ed9c5015632e0ad3c9705c84a4b6
Nicholas Moore <nicholas.s.moore@ttu.edu>
2019-10-13 21:27:05 -0500

    Add Vcycle Method, BuildLinearInterpOp, adjusted Jacobi for VCycle
    Jacobi now accepts (A, b, x0; numiter, weight) so that the options can 
    be passed as a named tuple to clean up VCyle code.

--- src/MooreJuliaMultigrid.jl
+++ src/MooreJuliaMultigrid.jl
@@ -1,22 +1,85 @@
 module MooreJuliaMultigrid
 
 using LinearAlgebra
+using SparseArrays
 
 """
-Run the Jacobi method on matrix system Ax=b for a total of numiters iterations.
-
-    Start with an initial guess of x0
+Run the Weighted Jacobi method on matrix system Ax=b for a total of numiters
+    iterations, starting with an initial guess of x0
 """
-function Jacobi(A,b,x0,numiters=1,weight=1)
+function Jacobi(A,b,x0; numiters=1,weight=1)
     wDinv = weight*inv(Diagonal(diag(A)))
     R = UniformScaling(1-weight) + wDinv*(A - Diagonal(diag(A)))
-    xnew = copy(x0)
     for i = 1:numiters
-        xnew = wDinv*b - R*xnew
+        x0 = wDinv*b - R*x0
     end
-    return xnew
+    return x0
 end
 
-export Jacobi
+"""
+V-cycle method to solve the matrix system Ax=b
+Inputs:
+    A, b, x0 : A matrix, RHS vector, initial guess
+    relaxFunc : Function to complete the relaxation
+      -- argument form : relaxFunc(A,b,x0; named options)
+    preRelaxOpts : Options for relaxFunc before coarse-grid transfer
+      -- named tuple with named options for relaxFunc
+    postRelaxOpts : Options for relaxFunc after coarse-grid correction
+      -- named tuple with named options for relaxFunc
+    prolongOp : Prolongation (interpolation) operator
+      -- matrix expected - may change
+    restrictOp : Restriction operator
+      -- matrix expected - may change
+"""
+function Vcycle(A,b,x0,relaxFunc,preRelaxOpts, postRelaxOpts, prolongOp, restrictOp)
+    # First relax numprerelax times
+    if preRelaxOpts[:numiters] > 0
+        x0 = relaxFunc(A,b,x0;preRelaxOpts...)
+    end
+
+    # Transfer residual to coarser grid
+    r = restrictOp*(b - A*x0)
+
+    # Transfer A to coarser grid
+    Acoarse = restrictOp*A*prolongOp
+
+    # Solve the coarse problem
+    ecourse = Acoarse \ r
+
+    # Interp ecourse to fine grid
+    e = prolongOp*ecourse
+
+    # Correct approximation with the residual
+    x0 = x0 + e
+
+    # Relax numpostrelax times
+    if postRelaxOpts[:numiters] > 0
+        x0 = relaxFunc(A,b,x0;postRelaxOpts...)
+    end
+    return x0
+end
+
+"""
+BuildLinearInterpOp builds the linear interpoltion operator for transferring
+    a vector of length fineSize to a vector of length (fineSize+1)/2 - 1.
+
+    Currently expects only an odd length vector
+
+    TODO: Add support for even length vectors
+"""
+function BuildLinearInterpOp(fineSize)
+    numCols::Int = (fineSize+1)/2 - 1
+    I = Vector{Int}(undef,3*numCols)
+    J = Vector{Int}(undef,3*numCols)
+    V = Vector{Float64}(undef,3*numCols)
+    for i = 1:numCols
+        I[3i-2:3i] = [2i-1; 2i; 2i+1]
+        J[3i-2:3i] = i*ones(3,1)
+        V[3i-2:3i] = [1; 2; 1]
+    end
+    return 0.5*sparse(I,J,V)
+end
+
+export Jacobi, Vcycle, BuildLinearInterpOp
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,23 @@ using LinearAlgebra
 using Test
 
 @testset "MooreJuliaMultigrid.jl" begin
-    # Write your own tests here.
-    n = 10
-    h = 1/n
-    d = 2*ones(n)
-    dl = -1*ones(n-1)
-    A = Tridiagonal(dl,d,dl)/h^2
-    b = zeros(n)
-    x0 = [.2*sin(3*x/n)+.7*sin(4*x/n)+.4*sin(10*x/n) for x = 1:n]
-    @test norm(Jacobi(A,b,x0,200,2/3)) < 0.001
+    # Matrix system to test
+    n = 128
+    h = 1/(n-1)
+    A = Tridiagonal(-1*ones(n-2),2*ones(n-1),-1*ones(n-2))/h^2
+    b = zeros(n-1)
+    x0 = [.2*sin(3*pi*x/n)+.7*sin(4*pi*x/n)+.4*sin(10*pi*x/n) for x = 1:(n-1)]
+    prolongOp = BuildLinearInterpOp(n-1)
+    restrictOp = 0.5*prolongOp'
+
+    @test norm(Jacobi(A,b,x0,numiters=10,weight=2/3)) < 10^(-2)
+    @test norm(BuildLinearInterpOp(7) - 0.5*[1 0 0;
+                                         2 0 0;
+                                         1 1 0;
+                                         0 2 0;
+                                         0 1 1;
+                                         0 0 2;
+                                         0 0 1]) == 0
+    @test norm(Vcycle(A,b,x0,Jacobi,(numiters=2,weight=2/3),(numiters=2,weight=2/3),prolongOp,restrictOp)) < 10^(-2)
+
 end
